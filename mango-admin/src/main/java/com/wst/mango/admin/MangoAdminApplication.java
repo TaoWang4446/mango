801efcc3648e4659b4e4980af426be2f40f00126
TaoWang4446 <2825586682@qq.com>
2020-07-21 10:41:07 +0800

    mango权限管理系统_服务注册中心consul

--- mango-admin/src/main/java/com/wst/mango/admin/MangoAdminApplication.java
+++ mango-admin/src/main/java/com/wst/mango/admin/MangoAdminApplication.java
@@ -2,8 +2,10 @@ package com.wst.mango.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MangoAdminApplication {
 
     public static void main(String[] args) {
--- mango-monitor/src/main/java/com/wst/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/wst/mango/monitor/MangoMonitorApplication.java
@@ -3,9 +3,11 @@ package com.wst.mango.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class MangoMonitorApplication {
 
     public static void main(String[] args) {
